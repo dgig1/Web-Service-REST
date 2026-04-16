020d0ac1eb416c6b8d9948b7d67cf96734b8a26e
dgig1 <dgig@outlook.com.br>
2026-04-16 01:48:05 +0000

    rest client

--- vagasws/src/main/java/vagasws/EmpresaController.java
+++ vagasws/src/main/java/vagasws/EmpresaController.java
@@ -1,52 +1,25 @@
 package vagasws;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EmpresaController {
-    private List<Empresa> empresas = new ArrayList<>();
-    public EmpresaController() {
-    empresas.add(new Empresa(1L, "Empresa Alfa LTDA", "12.345.678/0001-90", "contato@empresa-alfa.com"));
-    empresas.add(new Empresa(2L, "Beta Comércio ME", "98.765.432/0001-10", "beta@comercio.com"));
-    empresas.add(new Empresa(3L, "Gamma Serviços S.A.", "11.222.333/0001-44", "servicos@gamma.com"));
-    empresas.add(new Empresa(4L, "Delta Engenharia", "22.333.444/0001-55", "contato@deltaeng.com"));
-    empresas.add(new Empresa(5L, "Epsilon Digital", "33.444.555/0001-66", "email@epsilondigital.com"));
-    }
 
-    @GetMapping("/fci/api/empresas")
+    @Autowired
+    private EmpresaRepo empresaRepo;
+
+    @GetMapping("/vagasws/api/empresa")
     public Iterable<Empresa> getEmpresas() {
-        return empresas;
+        Iterable<Empresa> empresa = empresaRepo.findAll();
+        return empresa;
     }
 
-    @GetMapping("/fci/api/empresas/{id}")
-    public Empresa getEmpresa(@PathVariable long id) {
-        Empresa resposta = null;
-        for (Empresa e: empresas) {
-            if (e.getId() == id) {
-                resposta = e;
-                break;
-            }
+    @PostMapping("/vagasws/api/empresa")
+    public Empresa createEmpresa(@RequestBody Empresa e) {
+        return empresaRepo.save(e);
         }
-
-        return resposta;
-    }
-
-    @PostMapping("/fci/api/empresas")
-    public Empresa createEmpresa(@RequestBody Empresa novaEmpresa) {
-        long maiorId = 1;
-        for (Empresa e: empresas) {
-            if (e.getId() > maiorId) {
-                maiorId = e.getId();
-            }
-        }
-        long novoId = maiorId + 1;
-        novaEmpresa.setId(novoId);
-        empresas.add(novaEmpresa);
-        return novaEmpresa;
-    }
 }
---
+++ vagasws/src/main/java/vagasws/EmpresaRepo.java
@@ -0,0 +1,8 @@
+package vagasws;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EmpresaRepo 
+    extends CrudRepository<Empresa, Long>{
+    
+}
